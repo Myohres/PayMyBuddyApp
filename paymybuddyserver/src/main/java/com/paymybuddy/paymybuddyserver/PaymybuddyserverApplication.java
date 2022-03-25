21d8a8111c1f8043e98017fdb7ac628f70e9a6b1
Myohres <77498044+Myohres@users.noreply.github.com>
2022-03-25 16:28:06 +0100

    Initial Commit

---
+++ paymybuddyserver/src/main/java/com/paymybuddy/paymybuddyserver/PaymybuddyserverApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paymybuddyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymybuddyserverApplication.class, args);
+    }
+
+}
---
+++ paymybuddyserver/src/test/java/com/paymybuddy/paymybuddyserver/PaymybuddyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paymybuddyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
