484d2faefc0d16e272183960aaba1207b0bd6ffa
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-03 12:11:41 +0200

    checkstyle corectif web layer

--- paymybuddyserver/src/main/java/com/paymybuddy/paymybuddyserver/web/controller/TransactionBankController.java
+++ paymybuddyserver/src/main/java/com/paymybuddy/paymybuddyserver/web/controller/TransactionBankController.java
@@ -7,7 +7,11 @@ import com.paymybuddy.paymybuddyserver.web.mapper.TransactionMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.NoSuchElementException;
