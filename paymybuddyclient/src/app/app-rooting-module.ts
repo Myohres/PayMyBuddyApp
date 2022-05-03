9f61ef3ec1914b1463708b6d191105743992e1f9
Myohres <77498044+Myohres@users.noreply.github.com>
2022-05-03 21:36:02 +0200

    navigation path

--- paymybuddyclient/src/app/app-rooting-module.ts
+++ paymybuddyclient/src/app/app-rooting-module.ts
@@ -2,11 +2,15 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AuthenticationComponent} from "./authentication/authentication.component";
 import {SubscriptionComponent} from "./subscription/subscription.component";
+import {NavigationComponent} from "./navigation/navigation.component";
 
 const routes: Routes = [
   { path: '', component: AuthenticationComponent },
-  { path: 'transfer', component: AuthenticationComponent },
   { path: 'subscription', component: SubscriptionComponent},
+  { path:'home', component: NavigationComponent },
+  { path:'transfer', component: NavigationComponent},
+  { path:'profile', component: NavigationComponent },
+  { path:'contact', component: NavigationComponent },
 ];
 
 @NgModule({
